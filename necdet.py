bfb23d8d244657c87eaf1d58622d28d18cc9cfa6
ssf1_kraken <konpqwer@hotmail.com>
2016-12-22 03:24:06 +0300

    tubitaktan gelen öğrenci listesini csv yapınca hücrelerden değerleri alıp gerekli şekilde parçalayıp sıralı listeler oluşturuyor

--- necdet.py
+++ necdet.py
@@ -1,21 +1,76 @@
 import csv
 
+siralar = []
 
-def ogrenci_listesi():
-    liste = []
-    with open("olcumler.csv", "r") as olcumler:
-        reader = csv.DictReader(olcumler)
+isimler_ler = []
+isimler = []
+isimler2 = []
+
+danismanlar = []
+
+okullar_lar = []
+iller = []
+okullar = []
+
+alanlar = []
+
+def csv_ayirici():
+    with open('tubitak_listesi.csv', "r") as liste:
+        reader = csv.DictReader(liste)
         for satir in reader:
-            no = satir['proje no']
-            isim = satir['isim']
-            soyisim = satir['soy isim']
-            okul = satir['okul']
-            foto = satir['foto']
-            with open("sonuclar.txt", "w") as sonuclar:
-                print "Proje No:", " "*9, "isim"," "*21, "soyisim"," "*20, "okul"," "*20, "foto"
-                print "-"*6, " ", "-"*20, " "*2, "-"*20, " "*3, "-"*20, " ", "-"*20, " "*2, "-"*20, " "*2, "-"*20
-                print " ",no," "*5,isim," ",soyisim, okul, foto
+            sira = int(satir['sıra'])
+            bas_no = satir['BAŞVURU NO']
+            isim = satir['ÖĞRENCİ ADI SOYADI']
+            danisman = satir['DANIŞMAN ADI SOYADI']
+            okul = satir['OKULU']
+            proje = satir['PROJENİN ADI']
+            alani = satir['ALANI']
 
-ogrenci_listesi()
+            siralar.append(sira)
+            isimler_ler.append(isim)
+            danismanlar.append(danisman)
+            okullar_lar.append(okul)
+            alanlar.append(alani)
+
+def liste_ayiklayici():
+
+# ayni hucreden aldigi isimleri ayirdik
+    for i in isimler_ler:
+        isim1, isim2 = i.split("\n")
+        if isim2 != '':
+            isimler.append([isim1,isim2])
+        else:
+            isimler.append(isim1)
+
+
+
+# okul ve sehirleri ayirdik
+    for i in okullar_lar:
+        il, okul_ad = i.split(" ",1)
+        iller.append(il)
+
+# okul sonundaki kacis dizisinden kurtulduk
+    for i in okullar_lar:
+        okul_isim = i.split("\n")[0]
+        okullar.append(okul_isim)
+
+
+
+
+
+
+
+csv_ayirici()
+
+liste_ayiklayici()
+
+
+print (siralar)
+print (isimler)
+print (isimler2)
+print (danismanlar)
+print (iller)
+print (okullar)
+print (alanlar)
 
 
